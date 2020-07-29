cf33e39646c55cbff4a806eb7c489b1b57110279
malan88 <m@stdwtr.io>
2020-07-29 13:27:14 -0400

    Add styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Euler`,
+    description: `We are a conglomerate of expert engineers that are available on an hourly basis for consulting on your complex engineering tasks.`,
+    author: `@mas88`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
