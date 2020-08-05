b98e959f70d2c8d347a70042ebc9697faca31662
malan88 <m@stdwtr.io>
2020-08-05 12:36:31 -0400

    Re-arrange colors in theme to coolors palette

--- src/global/theme.js
+++ src/global/theme.js
@@ -5,14 +5,14 @@ export const theme = {
   red: "red",
   white: "white",
 
-  salmon: "#fca17d",
-  peach: "#F9DBBD",
-  taupe: "#785964",
-  morningBlue: "#82a7a6",
   black: "#000000",
-  gunmetal: "#113241",
   darkGunmetal: "#081920",
+  gunmetal: "#113241",
+  taupe: "#785964",
+  morningBlue: "#82A7A6",
+  salmon: "#FCA17D",
   lilac: "#B796AC",
+  peach: "#F9DBBD",
 
   get primary() {
     return this.white;
