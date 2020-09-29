f8526372ea1a6a426f43e11d1459de9d9b001525
malan88 <m@stdwtr.io>
2020-09-29 12:02:11 -0400

    Header link

--- src/components/header.js
+++ src/components/header.js
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 import Logo from '../assets/svg/euler.svg';
 
 const HeaderDiv = styled.nav`
-background-color: ${props => props.theme.morningBlue}60;
-padding: 1.5rem 0;
+background-color: ${props => props.shrink ? props.theme.darkGunmetal: props.theme.morningBlue + "60"};
+padding: ${props => props.shrink ? 0 : "1.5rem 0"};
 display: flex;
 position: fixed;
 width: 100%;
@@ -13,28 +13,30 @@ justify-content: center;
 z-index: 100;
 transition: padding ${props => props.theme.transition1} ease-in;
 transition: background-color ${props => props.theme.transition1} ease-in;
-
-a {
-  text-decoration: none;
-  color: ${props => props.theme.secondary};
-  font-family: Mulish;
-  text-transform: uppercase;
-  font-size: 0.9rem;
-  font-weight: bold;
-  letter-spacing: 3px;
-  margin: 1rem 3rem;
-  transition: color ${props => props.theme.transition1};
-  &:hover {
-    color: ${props => props.theme.highlight};
-  }
+`
+const NavLink = styled(Link)`
+text-decoration: none;
+color: ${props => props.theme.secondary};
+font-family: Mulish;
+text-transform: uppercase;
+font-size: 0.9rem;
+font-weight: bold;
+letter-spacing: 3px;
+margin: 1rem 3rem;
+transition: color ${props => props.theme.transition1};
+&:hover {
+  color: ${props => props.theme.highlight};
 }
 `
 const LogoStyled = styled(Logo)`
+margin-top: 0.5rem;
 margin-right: 5rem;
 fill: white;
 transition: width ${props => props.theme.transition1} linear;
 height: auto;
+width: ${props => props.shrink ? "3rem" : "5rem"}
 `
+
 const Header = ({ theme }) => {
   const [shrink, setShrink] = useState(false);
 
@@ -50,17 +52,16 @@ const Header = ({ theme }) => {
   useEffect(() => {
     window.addEventListener('scroll', onScroll);
   });
-  const shrunkStyle={ padding: "0", backgroundColor: "#081920" }
-  const style = shrink ? shrunkStyle : {};
-  const logoSize = shrink ? "3rem" : "5rem";
 
   return (
-    <HeaderDiv style={style}>
-      <LogoStyled style={{ width: logoSize }}/>
-      <Link to="/">Home</Link>
-      <Link to="/services">Services</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+    <HeaderDiv shrink={shrink}>
+      <Link to='/'>
+      <LogoStyled shrink={shrink}/>
+      </Link>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/services">Services</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
     </HeaderDiv>
   )
 }
