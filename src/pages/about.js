e01d949bc7696b189fc2f5c2bb65d60958cfb9d7
malan88 <m@stdwtr.io>
2020-09-29 13:07:03 -0400

    Add more text to about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -7,12 +8,36 @@ import { Highlight } from '../global/elements.js';
 import { Equation, EquationImg } from '../global/equation';
 import EquationPNG from '../images/equation.png';
 
+const About = styled.p`
+width: 800px;
+margin: 10rem auto;
+font-size: 2rem;
+line-height: 2.3rem;
+`
 
 const AboutPage = () => {
   return (
     <Layout>
       <SEO title="About" />
       <br /> {/* I don't know why but this is necessary to prevent page breakage */}
+      <About>
+        <Highlight>Euler Scientific</Highlight> is a partnership of software,
+        electrical, optics, and
+        mechanical engineers, and numerous other senior engineering
+        professionals.  We possess expertise in a number of disciplines,
+        including mechanical and electrical design, high performance computing,
+        power electronics, statistics, and both firmware and software.  We aim
+        to offer the highest possible quality of engineering services,
+        characterized by innovation, practicality, open and clear communication,
+        and integrity.
+      </About>
+      <About>
+        Our namesake, <Highlight>Leonhard Euler</Highlight>, whose picture
+        graces our landing page, was
+        one of the most prolific mathematicians of all time. His{' '}
+        <Highlight>insight</Highlight>{' '}
+        and <Highlight>creativity</Highlight> inspires us daily.
+      </About>
       <Equation>
         <EquationImg src={EquationPNG} />
         <p>
